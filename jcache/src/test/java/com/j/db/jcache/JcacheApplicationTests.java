b9fcb01f8a939f61c5e5035c42705dde4d5d99a0
jayant.kumar <jayant.kumar@op.fi>
2024-11-18 19:36:48 +0530

    Basic structure for jcache application

---
+++ jcache/src/test/java/com/j/db/jcache/JcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j.db.jcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
