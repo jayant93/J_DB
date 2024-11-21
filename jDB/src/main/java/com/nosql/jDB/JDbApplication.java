4092d8b06b14f74e3ae5e7eab173dee05d9a13cd
jayant.kumar <jayant.kumar@op.fi>
2024-11-21 16:47:12 +0530

    spring boot app for nosql jdb added

---
+++ jDB/src/main/java/com/nosql/jDB/JDbApplication.java
@@ -0,0 +1,13 @@
+package com.nosql.jDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDbApplication.class, args);
+	}
+
+}
---
+++ jDB/src/test/java/com/nosql/jDB/JDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosql.jDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
